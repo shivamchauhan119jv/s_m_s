4306f1400b225667564a019eff6ff05d33139116
shivam <you@example.com>
2024-09-07 02:51:44 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeNavbarComponent } from './home/home-navbar/home-navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeNavbarComponent }
+];
---
+++ src/app/home/home-navbar/home-navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms'; // Import FormsModul
+
+@Component({
+  selector: 'app-home-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-navbar.component.html',
+  styleUrl: './home-navbar.component.css'
+})
+export class HomeNavbarComponent {
+
+  
+
+  // onSubmit() {
+  //   if (this.myForm.valid) {
+  //     console.log('Form Submitted!', this.myForm.value);
+  //   }
+  // }
+
+}
